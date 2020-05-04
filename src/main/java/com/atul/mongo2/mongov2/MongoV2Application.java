e051e71d2830f95ced661223aa1ac51fa166ac07
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-04 09:04:22 +0530

    Initial commit

---
+++ src/main/java/com/atul/mongo2/mongov2/MongoV2Application.java
@@ -0,0 +1,13 @@
+package com.atul.mongo2.mongov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/atul/mongo2/mongov2/MongoV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.mongo2.mongov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
